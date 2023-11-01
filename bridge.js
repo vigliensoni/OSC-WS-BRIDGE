ee2fc38a0a2c16db067fe874d4af07c2fa41a5e5
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2023-11-01 14:50:24 -0400

    Add: Specify host.

--- bridge.js
+++ bridge.js
@@ -1,6 +1,6 @@
 const OSC = require('osc-js')
-
-const config = { udpClient: { port: 9129 } }
+// For more settings, check https://doc.esdoc.org/github.com/adzialocha/osc-js/class/src/plugin/bridge.js~BridgePlugin.html
+const config = { udpClient: { port: 9129, host: "localhost" } }
 const osc = new OSC({ plugin: new OSC.BridgePlugin(config) })
 
 osc.open() // start a WebSocket server on port 8080
