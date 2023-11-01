2a3bb3d94cbc0894de164d96712b0b10236164e6
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2023-11-01 15:49:10 -0400

    Add: Change to default address.

--- bridge.js
+++ bridge.js
@@ -1,7 +1,7 @@
 const OSC = require('osc-js')
 // For more settings, check https://doc.esdoc.org/github.com/adzialocha/osc-js/class/src/plugin/bridge.js~BridgePlugin.html
 const config = { wsServer: { host: "localhost", port: 8080 },
-                 udpClient: { host: "localhost", port: 9129 } }
+                 udpClient: { host: "localhost", port: 41235 } }
 const osc = new OSC({ plugin: new OSC.BridgePlugin(config) })
 
 osc.open() // start the WebSocket server 
